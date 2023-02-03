7cfbb649f3eaf8adacdd407307f0dc39edbec3a5
caiocfournier <caiocfournier@gmail.com>
2023-02-03 16:38:15 -0300

    Indenta código do jogo do coelho

--- Project-Template-C12-main/sketch.js
+++ Project-Template-C12-main/sketch.js
@@ -53,26 +53,26 @@ function draw() {
 }
 
 function createApples() {
-apple = createSprite(random(50, 350),40, 10, 10);
-apple.addImage(appleImg);
-apple.scale=0.07;
-apple.velocityY = 3;
-apple.lifetime = 150;
+  apple = createSprite(random(50, 350),40, 10, 10);
+  apple.addImage(appleImg);
+  apple.scale=0.07;
+  apple.velocityY = 3;
+  apple.lifetime = 150;
   
 }
 
 function createOrange() {
-orangeL = createSprite(random(50, 350),40, 10, 10);
-orangeL.addImage(orangeImg);
-orangeL.scale=0.08;
-orangeL.velocityY = 3;
-orangeL.lifetime = 150;
+  orangeL = createSprite(random(50, 350),40, 10, 10);
+  orangeL.addImage(orangeImg);
+  orangeL.scale=0.08;
+  orangeL.velocityY = 3;
+  orangeL.lifetime = 150;
 }
 
 function createRed() {
-redL = createSprite(random(50, 350),40, 10, 10);
-redL.addImage(redImg);
-redL.scale=0.06;
+  redL = createSprite(random(50, 350),40, 10, 10);
+  redL.addImage(redImg);
+  redL.scale=0.06;
   redL.velocityY = 3;
   redL.lifetime = 150;
 }
