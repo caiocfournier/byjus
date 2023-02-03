f0cc5287c0f998da6b3b41d49f7c8ce4b29c6dcd
caiocfournier <caiocfournier@gmail.com>
2023-02-03 16:01:16 -0300

    Coloca cactos no jogo do trex

--- Trex-Etapa-0.5-main/sketch.js
+++ Trex-Etapa-0.5-main/sketch.js
@@ -1,14 +1,22 @@
-var trex, trex_correndo;
-var chao, chao_imagem, chao_invisivel;
-var nuvem, nuvem_imagem;
+ var trex, trex_correndo;
+ var chao, chao_imagem, chao_invisivel;
+ var nuvem, nuvem_imagem;
+ var obstaculo, obstaculo1, obstaculo2, obstaculo3,
+ obstaculo4, obstaculo5, obstaculo6;
 
-function preload(){
+ function preload(){
   trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   chao_imagem = loadImage("ground2.png");
-  nuvem_imagem = loadImage("cloud.png")
-}
+  nuvem_imagem = loadImage("cloud.png");
 
-function setup(){
+  obstaculo1 = loadImage("obstacle1.png");
+  obstaculo2 = loadImage("obstacle2.png");
+  obstaculo3 = loadImage("obstacle3.png");
+  obstaculo5 = loadImage("obstacle5.png");
+  obstaculo6 = loadImage("obstacle6.png");
+ }
+
+ function setup(){
   createCanvas(600, 200);
  
   trex = createSprite(50, 160, 20, 50);
@@ -21,16 +29,16 @@ function setup(){
 
   chao_invisivel = createSprite(200, 190, 400, 10);
   chao_invisivel.visible = false;
-}
+ }
 
-function draw(){
+ function draw(){
   background(255);
 
- chao.velocityX = -5;
+ chao.velocityX = -6;
 
-if(chao.x < 0){
+ if(chao.x < 0){
   chao.x = chao.width / 2;
-}
+ }
 
 
  if(keyDown("space") && trex.y >= 100){
@@ -40,21 +48,50 @@ if(chao.x < 0){
   trex.collide(chao_invisivel);
 
   criarNuvens();
-
+  criarObstaculos();
   drawSprites();
 
   console.log(frameCount);
-}
+ }
 
 function criarNuvens(){
-  if(frameCount % 60 === 0){
+   if(frameCount % 60 === 0){
     nuvem = createSprite(600, 100, 40, 10);
     nuvem.addImage(nuvem_imagem);
     nuvem.y = Math.round(random(10,100));
     nuvem.scale = 0.7;
     nuvem.velocityX = -3; 
+    
+    nuvem.lifetime = 200;
 
     nuvem.depth = trex.depth;
     trex.depth = trex.depth + 1; 
   }
-}
+ }
+
+ function criarObstaculos(){
+  if(frameCount % 60 === 0){
+    obstaculo = createSprite(400, 165, 10, 40);
+    obstaculo.velocityX = -6;
+    
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstaculo.addImage(obstaculo1);
+              break;
+      case 2: obstaculo.addImage(obstaculo2);
+              break;
+      case 3: obstaculo.addImage(obstaculo3);
+              break;
+      case 4: obstaculo.addImage(obstaculo4);
+              break;
+      case 5: obstaculo.addImage(obstaculo5);
+              break;
+      case 6: obstaculo.addImage(obstaculo6);
+              break;
+      default: break;
+    }
+
+    obstaculo.scale = 0.5;
+    obstaculo.lifetime = 100;
+  }
+ }  
