a125d5dd912f1a4869d9e8749c15f2857223ef29
caiocfournier <caiocfournier@gmail.com>
2023-02-07 11:00:00 -0300

    Adiciona a tela de game over do jogo do trex

--- Trex-Etapa-0.5-main/sketch.js
+++ Trex-Etapa-0.5-main/sketch.js
@@ -1,19 +1,30 @@
- var trex, trex_correndo;
+ var PLAY = 1;
+ var END = 0;
+ var estadoDeJogo = PLAY;
+ var trex, trex_correndo, trex_colidindo;
  var chao, chao_imagem, chao_invisivel;
  var nuvem, nuvem_imagem;
  var obstaculo, obstaculo1, obstaculo2, obstaculo3,
  obstaculo4, obstaculo5, obstaculo6;
+ var pontos;
+ var grupo_obstaculo, grupo_nuvem;
+ var gameOver_imagem, restart_imagem;
 
  function preload(){
   trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  trex_colidindo = loadAnimation("trex_collided.png");
   chao_imagem = loadImage("ground2.png");
   nuvem_imagem = loadImage("cloud.png");
 
   obstaculo1 = loadImage("obstacle1.png");
   obstaculo2 = loadImage("obstacle2.png");
   obstaculo3 = loadImage("obstacle3.png");
+  obstaculo4 = loadImage("obstacle4.png");
   obstaculo5 = loadImage("obstacle5.png");
   obstaculo6 = loadImage("obstacle6.png");
+
+  gameOver_imagem = loadImage("gameOver.png");
+  restart_imagem= loadImage("restart.png");
  }
 
  function setup(){
@@ -21,6 +32,7 @@
  
   trex = createSprite(50, 160, 20, 50);
   trex. addAnimation("correndo", trex_correndo);
+  trex.addAnimation("colidindo", trex_colidindo);
   trex.scale = 0.5;
 
   chao = createSprite(200, 180, 400, 20);
@@ -29,26 +41,62 @@
 
   chao_invisivel = createSprite(200, 190, 400, 10);
   chao_invisivel.visible = false;
+
+  gameOver = createSprite(300, 100);
+  gameOver.addImage(gameOver_imagem);
+  gameOver.scale = 0.5;
+
+  restart= createSprite(300, 140);
+  restart.addImage(restart_imagem);
+  restart.scale = 0.5;
+
+
+  grupo_obstaculo = new Group();
+  grupo_nuvem = new Group();
+  pontos = 0;
  }
 
  function draw(){
   background(255);
+  text("pontuação: " + pontos, 500, 50);
+  trex.setCollider("circle", 0, 0, 40);
+  
+  if(estadoDeJogo === PLAY){
+    gameOver.visible = false;
+    restart.visible = false;
+     pontos = pontos + Math.round(frameCount/60);
+     chao.velocityX = -6;
+     if(chao.x < 0){
+     chao.x = chao.width / 2;
+       }
+      if(keyDown("space") && trex.y >= 100){
+        trex.velocityY = -10;
+       }
+       trex.velocityY = trex.velocityY + 0.5;  
+       criarNuvens();
+       criarObstaculos();
 
- chao.velocityX = -6;
-
- if(chao.x < 0){
-  chao.x = chao.width / 2;
- }
-
-
- if(keyDown("space") && trex.y >= 100){
-   trex.velocityY = -10;
+       if(grupo_obstaculo.isTouching(trex)){
+        estadoDeJogo = END;
+       }
   }
-  trex.velocityY = trex.velocityY + 0.5;
+  else if(estadoDeJogo === END){
+      gameOver.visible = true;
+      restart.visible = true;
+    chao.velocityX = 0;
+    trex.velocityY = 0;
+    grupo_nuvem.setVelocityXEach(0);
+    grupo_obstaculo.setVelocityXEach(0);
+
+    trex.changeAnimation("colidindo", trex_colidindo);
+
+    grupo_nuvem.setLifetimeEach(-1);
+    grupo_obstaculo.setLifetimeEach(-1);
+  }
+
   trex.collide(chao_invisivel);
 
-  criarNuvens();
-  criarObstaculos();
+  
   drawSprites();
 
   console.log(frameCount);
@@ -66,6 +114,8 @@ function criarNuvens(){
 
     nuvem.depth = trex.depth;
     trex.depth = trex.depth + 1; 
+
+    grupo_nuvem.add(nuvem);
   }
  }
 
@@ -93,5 +143,7 @@ function criarNuvens(){
 
     obstaculo.scale = 0.5;
     obstaculo.lifetime = 100;
+
+    grupo_obstaculo.add(obstaculo);
   }
  }  
