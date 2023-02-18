bac97ca9f69b6052901b6a613447e07cbfa8eb00
caiocfournier <caiocfournier@gmail.com>
2023-02-17 22:09:22 -0300

    Correções no código do projeto das formas cadentess

--- PRO-C20-V2/sketch.js
+++ PRO-C20-V2/sketch.js
@@ -8,6 +8,8 @@ var block1;
 var block2;
 var block3;
 var plane;
+var engine;
+var world;
 
 function preload()
 {
@@ -15,9 +17,10 @@ function preload()
 }
 
 function setup() {
-	createCanvas(548, 600);
+	var canvas = createCanvas(550,600);
 	engine = Engine.create();
 	world = engine.world;
+	fill("#991E2A")
 
 	var block1_options = {
 		restitution: 0.5,
@@ -42,14 +45,14 @@ function setup() {
 	  }
 
 	//Crie os Corpos Aqui.
-	block1 = Bodies.circle(220,10,10,block1_options);
+	block1 = Bodies.circle(220,10,23,block1_options);
 	World.add(world, block1);
 
 	block2 = Bodies.rectangle(110,50,10,25,block2_options);
 	World.add(world, block2);
 
 	block3 = Bodies.rectangle(350,50,10,25,block3_options);
-	World.add(world, block3);
+	World.add(world, block3);   
 
 	plane = Bodies.rectangle(300,600,500,25,plane_options);
   	World.add(world, plane);
@@ -63,11 +66,10 @@ function draw() {
 	ellipseMode(RADIUS);
 	background("#95FC91");
 	
-	drawSprites();
 	Engine.update(engine);
 
-	ellipse(block1.position.x, block1.position.y, 20);
-	rect(block2.position.x, block2.position.y, 50);
+	ellipse(block1.position.x, block1.position.y, 35);
+	rect(block2.position.x, block2.position.y, 50, 50);
 	rect(block3.position.x, block3.position.y, 100, 50);
 	
 	rect(plane.position.x, plane.position.y, 600, 1);
