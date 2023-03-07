03abd46211dae2b5f9ab7c02b6dcfe4371f05b18
caiocfournier <caiocfournier@gmail.com>
2023-03-07 19:07:24 -0300

    Adiciona código do projeto bolas de papel amassadas

---
+++ PRO - 21 V2/sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Vector = Matter.Vector;
+
+var ball, groundObj, leftSide, rightSide, rightWall;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	var ball_options = {
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
+	};
+
+	//Create the Bodies Here.
+	engine = Engine.create();
+	world = engine.world;
+
+	groundObj = new Ground(width/2, 920, width, 10);
+	leftSide = new Ground(1300, 860, 10, 120);
+	rightSide = new Ground(1500, 860, 10, 120);
+	rightWall = new Ground(width-5, 860, 10, height*2);
+
+	ball = Bodies.circle(200, 100, 20, ball_options);
+    World.add(world, ball);
+
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
+
+
+	Engine.run(engine);
+  
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Body.applyForce(ball, Vector.create(20,50), Vector.create(50,20));
+	}
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  fill("#fff");
+  groundObj.display();
+  leftSide.display();
+  rightSide.display();
+  rightWall.display();
+
+  ellipse(ball.position.x, ball.position.y, 20);
+  
+  drawSprites();
+
+  Engine.update(engine);
+}
+
+
+
