22092e287e3bb846e7e7010cd217eb5bb0e5d171
caiocfournier <caiocfournier@gmail.com>
2023-04-11 20:50:19 -0300

    adiciona codigo do jogo do alimente do coelho

--- alimente-coelho-Caio-main/sketch.js
+++ alimente-coelho-Caio-main/sketch.js
@@ -22,10 +22,25 @@ var rabbit;
 var bunny;
 var button;
 
+var blink,
+    eat,
+    sad;
+
 function preload() {
     bg_img = loadImage("background.png");
     food = loadImage("melon.png");
     rabbit = loadImage("Rabbit-01.png");
+
+    blink = loadAnimation("blink_1.png", "blink_2.png", "blink_3.png");
+    eat = loadAnimation("eat_0.png", "eat_1.png", "eat_2.png", "eat_3.png", "eat_4.png");
+    sad = loadAnimation("sad_1.png", "sad_2.png", "sad_3.png");
+
+    blink.playing = true;
+    eat.playing = true;
+    sad.playing = true;
+
+    sad.looping = false;
+    eat.looping = false;
 }
 function setup() {
     createCanvas(500, 700);
@@ -43,9 +58,18 @@ function setup() {
     button.size(50, 50);
     button.mouseClicked(drop);
 
+    blink.frameDelay = 15;
+    eat.frameDelay = 20;
+    sad.frameDelay = 20;
+
     bunny = createSprite(250, 625, 100, 100);
-    bunny.addImage(rabbit);
     bunny.scale = 0.2;
+
+    bunny.addAnimation("blinking", blink);
+    bunny.addAnimation("eating", eat);
+    bunny.addAnimation("crying", sad);
+
+    bunny.changeAnimation("blinking");
 
     var fruit_options = {
         density: 0.001
@@ -69,8 +93,17 @@ function draw() {
     ground.show();
     rope.show();
 
-    image(food, fruit.position.x, fruit.position.y, 60, 60);
+    if(fruit !=null){
+        image(food, fruit.position.x, fruit.position.y, 60, 60);
+    }
 
+    if(collided(fruit, bunny) == true){
+        bunny.changeAnimation("eating");
+    }
+    if(collided(fruit, ground.body) == true){
+        bunny.changeAnimation("crying");
+    }
+  
     Engine.update(engine);
     drawSprites();
 }
@@ -79,4 +112,18 @@ function drop() {
     rope.break();
     fruit_con.detach();
     fruit_con = null;
+}
+
+function collided(body, sprite){
+    if( body != null){
+        var d = dist(body.position.x, body.position.y, sprite.position.x, sprite.position.y);
+        if(d <= 80){
+            World.remove(engine.world, fruit);
+            fruit = null;
+            return true;
+        } else{
+            return false;
+
+        }
+    }
 }
