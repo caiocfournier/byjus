01d543f00508ba055ceb59f10e4e8b09428b27fc
caiocfournier <caiocfournier@gmail.com>
2023-04-23 12:41:43 -0300

    adiciona codigo do jogo alimente o coelho

--- alimente-coelho-Caio-main - Copia/sketch.js
+++ alimente-coelho-Caio-main - Copia/sketch.js
@@ -11,22 +11,16 @@ let engine;
 let world;
 
 var ground;
-var rope,
-    rope2,
-    rope3;
+var rope;
 var fruit;
-var fruit_con,
-    fruit_con2,
-    fruit_con3;
+var fruit_con;
 
 var bg_img;
 var food;
 var rabbit;
 
 var bunny;
-var button,
-    button2,
-    button3;
+var button;
 var ballon;
 var mute_btn;
 
@@ -39,9 +33,6 @@ var cut_sound;
 var sad_sound;
 var eating_sound;
 var air_sound;
-
-var canW,
-    canH;
 
 function preload() {
     bg_img = loadImage("background.png");
@@ -66,17 +57,7 @@ function preload() {
     eat.looping = false;
 }
 function setup() {
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-        canW = displayWidth;
-        canH = displayHeight;
-        createCanvas(canW + 80, canH);
-    } else {
-        canW = windowWidth;
-        canH = windowHeight;
-        createCanvas(canW, canH);
-    }
-    
+    createCanvas(500, 700);
 
     bg_sound.play();
     bg_sound.setVolume(0.5);
@@ -84,45 +65,32 @@ function setup() {
     engine = Engine.create();
     world = engine.world;
 
-    ground = new Ground(200, canH, 600, 20);
-    rope = new Rope(9, {
-        x: 40,
+    ground = new Ground(200, 690, 600, 20);
+    rope = new Rope(6, {
+        x: 245,
         y: 30
-    });
-    rope2 = new Rope(7, {
-        x: 370,
-        y: 40
-    });
-    rope3 = new Rope(4, {
-        x: 400,
-        y: 220
     });
 
     button = createImg("cut_btn.png");
-    button.position(20, 30);
+    button.position(220, 30);
     button.size(50, 50);
     button.mouseClicked(drop);
-
-    button2 = createImg("cut_btn.png");
-    button2.position(330, 35);
-    button2.size(50, 50);
-    button2.mouseClicked(drop2);
-
-    button3 = createImg("cut_btn.png");
-    button3.position(360, 200);
-    button3.size(50, 50);
-    button3.mouseClicked(drop3);
 
     mute_btn = createImg("mute.png");
     mute_btn.position(450, 20);
     mute_btn.size(50, 50);
     mute_btn.mouseClicked(mute);
 
+    ballon = createImg("balloon.png");
+    ballon.position(10, 195);
+    ballon.size(150, 100);
+    ballon.mouseClicked(airballon);
+
     blink.frameDelay = 15;
     eat.frameDelay = 20;
     sad.frameDelay = 20;
 
-    bunny = createSprite(100, canH - 80, 100, 100);
+    bunny = createSprite(420, 625, 100, 100);
     bunny.scale = 0.2;
 
     bunny.addAnimation("blinking", blink);
@@ -139,8 +107,6 @@ function setup() {
     Composite.add(rope.body, fruit);
 
     fruit_con = new Link(rope, fruit);
-    fruit_con2 = new Link(rope2, fruit);
-    fruit_con3 = new Link(rope3, fruit);
 
     rectMode(CENTER);
     ellipseMode(RADIUS);
@@ -150,12 +116,10 @@ function setup() {
 
 function draw() {
     background(51);
-    image(bg_img, 0, 0, canW, canH );
+    image(bg_img, width / 2, height / 2, 500, 700);
 
     ground.show();
     rope.show();
-    rope2.show();
-    rope3.show();
 
     if (fruit != null) {
         image(food, fruit.position.x, fruit.position.y, 60, 60);
@@ -165,7 +129,7 @@ function draw() {
         bunny.changeAnimation("eating");
         eating_sound.play()
     }
-    if (fruit != null && fruit.position.y >= 650) {
+    if (fruit != null &&fruit.position.y >= 650) {
         bunny.changeAnimation("crying");
         bg_sound.stop();
         sad_sound.play();
@@ -183,21 +147,6 @@ function drop() {
     fruit_con = null;
 }
 
-function drop2() {
-    cut_sound.play();
-    rope2.break();
-    fruit_con2.detach();
-    fruit_con2 = null;
-}
-
-function drop3() {
-    cut_sound.play();
-    rope3.break();
-    fruit_con3.detach();
-    fruit_con3 = null;
-}
-
-
 function collided(body, sprite) {
     if (body != null) {
         var d = dist(body.position.x, body.position.y, sprite.position.x, sprite.position.y);
@@ -212,9 +161,19 @@ function collided(body, sprite) {
     }
 }
 
+function airballon() {
+    Body.applyForce(fruit, {
+        x: 0,
+        y: 0
+    }, {
+        x: 0.01,
+        y: 0
+    });
+   air_sound.play() 
+}
 
-function mute() {
-    if (bg_sound.isPlaying()) {
+function mute(){
+    if(bg_sound.isPlaying()){
         bg_sound.stop();
     } else {
         bg_sound.play();
