79526e984711ffeb65cdc47ed34b0994cf7c1956
caiocfournier <caiocfournier@gmail.com>
2023-05-14 21:59:39 -0300

    Adiciona código do jogo do coelho

--- alimente-coelho-Caio-main/sketch.js
+++ alimente-coelho-Caio-main/sketch.js
@@ -12,12 +12,10 @@ let world;
 
 var ground;
 var rope,
-    rope2,
-    rope3;
+    rope2;
 var fruit;
 var fruit_con,
-    fruit_con2,
-    fruit_con3;
+    fruit_con2;
 
 var bg_img;
 var food;
@@ -25,8 +23,7 @@ var rabbit;
 
 var bunny;
 var button,
-    button2,
-    button3;
+    button2;
 var ballon;
 var mute_btn;
 
@@ -40,8 +37,7 @@ var sad_sound;
 var eating_sound;
 var air_sound;
 
-var canW,
-    canH;
+var star_img, star1, star2;
 
 function preload() {
     bg_img = loadImage("background.png");
@@ -58,6 +54,8 @@ function preload() {
     eat = loadAnimation("eat_0.png", "eat_1.png", "eat_2.png", "eat_3.png", "eat_4.png");
     sad = loadAnimation("sad_1.png", "sad_2.png", "sad_3.png");
 
+    star_img = loadImage("star.png")
+
     blink.playing = true;
     eat.playing = true;
     sad.playing = true;
@@ -66,17 +64,8 @@ function preload() {
     eat.looping = false;
 }
 function setup() {
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-        canW = displayWidth;
-        canH = displayHeight;
-        createCanvas(canW + 80, canH);
-    } else {
-        canW = windowWidth;
-        canH = windowHeight;
-        createCanvas(canW, canH);
-    }
     
+  createCanvas(600, 700);
 
     bg_sound.play();
     bg_sound.setVolume(0.5);
@@ -84,37 +73,27 @@ function setup() {
     engine = Engine.create();
     world = engine.world;
 
-    ground = new Ground(200, canH, 600, 20);
-    rope = new Rope(9, {
-        x: 40,
-        y: 30
+    ground = new Ground(300, height, width, 20);
+    rope = new Rope(7, {
+        x: 120,
+        y: 90
     });
     rope2 = new Rope(7, {
-        x: 370,
-        y: 40
+        x: 490,
+        y: 90
     });
-    rope3 = new Rope(4, {
-        x: 400,
-        y: 220
-    });
-
     button = createImg("cut_btn.png");
-    button.position(20, 30);
+    button.position(100, 90);
     button.size(50, 50);
     button.mouseClicked(drop);
 
     button2 = createImg("cut_btn.png");
-    button2.position(330, 35);
+    button2.position(450, 90);
     button2.size(50, 50);
     button2.mouseClicked(drop2);
 
-    button3 = createImg("cut_btn.png");
-    button3.position(360, 200);
-    button3.size(50, 50);
-    button3.mouseClicked(drop3);
-
     mute_btn = createImg("mute.png");
-    mute_btn.position(450, 20);
+    mute_btn.position(width-50, 20);
     mute_btn.size(50, 50);
     mute_btn.mouseClicked(mute);
 
@@ -122,7 +101,7 @@ function setup() {
     eat.frameDelay = 20;
     sad.frameDelay = 20;
 
-    bunny = createSprite(100, canH - 80, 100, 100);
+    bunny = createSprite(100, height - 80, 100, 100);
     bunny.scale = 0.2;
 
     bunny.addAnimation("blinking", blink);
@@ -130,6 +109,19 @@ function setup() {
     bunny.addAnimation("crying", sad);
 
     bunny.changeAnimation("blinking");
+
+    star1 = createSprite(320, 50, 20, 20);
+    star1.addImage(star_img);
+    star1.scale = 0.02;
+
+    star2 = createSprite(50, 330, 20, 20);
+    star2.addImage(star_img);
+    star2.scale = 0.02;
+
+    ballon = createImg("baloon2.png")
+    ballon.position(260,370);
+    ballon.size(120,120);
+    ballon.mouseClicked(airBallon);
 
     var fruit_options = {
         density: 0.001
@@ -140,7 +132,6 @@ function setup() {
 
     fruit_con = new Link(rope, fruit);
     fruit_con2 = new Link(rope2, fruit);
-    fruit_con3 = new Link(rope3, fruit);
 
     rectMode(CENTER);
     ellipseMode(RADIUS);
@@ -150,18 +141,19 @@ function setup() {
 
 function draw() {
     background(51);
-    image(bg_img, 0, 0, canW, canH );
+    image(bg_img, width/2, height/2, width, height);
 
     ground.show();
     rope.show();
     rope2.show();
-    rope3.show();
 
     if (fruit != null) {
         image(food, fruit.position.x, fruit.position.y, 60, 60);
     }
 
-    if (collided(fruit, bunny) == true) {
+    if (collided(fruit, bunny, 80) == true){
+        World.remove(engine.world, fruit);
+        fruit = null;
         bunny.changeAnimation("eating");
         eating_sound.play()
     }
@@ -172,6 +164,13 @@ function draw() {
         fruit = null;
     }
 
+    if (collided(fruit, star1, 20) == true){
+      star1.visible = false
+    }
+
+    if (collided(fruit, star2, 20) == true){
+        star2.visible = false
+      }
     Engine.update(engine);
     drawSprites();
 }
@@ -190,22 +189,12 @@ function drop2() {
     fruit_con2 = null;
 }
 
-function drop3() {
-    cut_sound.play();
-    rope3.break();
-    fruit_con3.detach();
-    fruit_con3 = null;
-}
-
-
-function collided(body, sprite) {
+function collided(body, sprite, x) {
     if (body != null) {
         var d = dist(body.position.x, body.position.y, sprite.position.x, sprite.position.y);
-        if (d <= 80) {
-            World.remove(engine.world, fruit);
-            fruit = null;
+        if (d <= x) {
             return true;
-        } else {
+          } else {
             return false;
 
         }
@@ -219,4 +208,9 @@ function mute() {
     } else {
         bg_sound.play();
     }
+}
+
+function airBallon(){
+    Body.applyForce(fruit, {x:0, y:0}, {x:0, y:-0.03});
+    air_sound.play();
 }
