a36fc6fc13bd0041f1917749671b16e7bf7dc477
caiocfournier <caiocfournier@gmail.com>
2023-10-01 19:59:13 -0300

    Adiciona código do projeto da biblioteca

---
+++ biblioteca/screens/Search.js
@@ -0,0 +1,30 @@
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Component} from 'react/cjs/react.production.min';
+
+export default class SearchScreen extends Component {
+    render() {
+        return (
+            <View style={
+                styles.container
+            }>
+                <Text style={
+                    styles.text
+                }>Tela de Pesquisa</Text>
+                <StatusBar style="auto"/>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: "black",
+        fontSize: 30
+    }
+});
