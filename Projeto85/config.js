bd92394980d796b06f44fc9cb9618beb3f23e2ef
caiocfournier <caiocfournier@gmail.com>
2023-12-19 14:28:08 -0300

    Atualiza código do projeto 85

--- Projeto85/config.js
+++ Projeto85/config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAc-1j3xaFx1fcgoMWVOfOEl2TnJWRjBy8",
   authDomain: "aplicativo-espectograma-70c1c.firebaseapp.com",
   databaseURL: "https://aplicativo-espectograma-70c1c-default-rtdb.firebaseio.com",
@@ -13,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "257011407903",
   appId: "1:257011407903:web:4e4a8642caa5309c5cc92a"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
