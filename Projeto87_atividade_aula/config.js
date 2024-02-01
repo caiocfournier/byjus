bc58190495ef600554688fca4a3f7289c73b49ec
caiocfournier <caiocfournier@gmail.com>
2024-02-01 15:01:42 -0300

    Adiciona codigo do espectagrama parte 7

---
+++ Projeto87_atividade_aula/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAc-1j3xaFx1fcgoMWVOfOEl2TnJWRjBy8",
+  authDomain: "aplicativo-espectograma-70c1c.firebaseapp.com",
+  databaseURL: "https://aplicativo-espectograma-70c1c-default-rtdb.firebaseio.com",
+  projectId: "aplicativo-espectograma-70c1c",
+  storageBucket: "aplicativo-espectograma-70c1c.appspot.com",
+  messagingSenderId: "257011407903",
+  appId: "1:257011407903:web:4e4a8642caa5309c5cc92a"
+};
