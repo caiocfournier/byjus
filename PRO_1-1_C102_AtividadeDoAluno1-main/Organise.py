cd6004b69f0c94f6fb8bd2809c18100f26fdbf1c
caiocfournier <caiocfournier@gmail.com>
2024-06-16 18:40:52 -0300

    Adiciona  o codigo da atividade do aluno 102

---
+++ PRO_1-1_C102_AtividadeDoAluno1-main/Organise.py
@@ -0,0 +1,6 @@
+import os 
+import shutil 
+from_dir = "C://Users//caioc//Documents//projetos//byjus//Torre do pirata"
+to_dir = "C://Users//caioc//Documents//projetos//byjus//Torre do pirata"
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
