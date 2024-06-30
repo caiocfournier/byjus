7d99136730bde5ab092f2a0aaeee3025547c3934
caiocfournier <caiocfournier@gmail.com>
2024-06-30 12:55:27 -0300

    Adiciona aula do projeto 107

---
+++ PRO_1-1_C107_AtividadeDoAluno1-main - Copia/object_tracking.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+import math
+
+video = cv2.VideoCapture("bb3.mp4")
+
+while True:
+    
+    check, img = video.read()   
+
+    cv2.imshow("resultado", img)
+            
+    if key == 32:
+        print("Interrompido")
+        break
+
+video.release()
+cv2.destroyALLwindows()
